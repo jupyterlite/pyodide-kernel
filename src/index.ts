3d326269fd29076097f15082f00d847aa4eaf0e0
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-12-01 15:28:13 +0100

    Lint

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ const plugin: JupyterLiteServerPlugin<void> = {
   id: '@jupyterlite/pyodide-kernel:plugin',
   autoStart: true,
   activate: (app: JupyterLiteServer) => {
-    console.log('JupyterLite server extension @jupyterlite/pyodide-kernel is activated!');
+    console.log(
+      'JupyterLite server extension @jupyterlite/pyodide-kernel is activated!'
+    );
   }
 };
 
