b977e25416f8f98e6ff7371b8320e25ad8d75254
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-03-04 09:59:43 +0000

    Build explicitly on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -24,3 +24,18 @@ jupyterlite_dir = "."
 #       "image_dark": "TODO",
 #    }
 # }
+
+def on_config_inited(*args):
+    import sys
+    import subprocess
+    from pathlib import Path
+
+    HERE = Path(__file__)
+    ROOT = HERE.parent.parent
+    subprocess.check_call(["yarn"], cwd=str(ROOT))
+    subprocess.check_call(["yarn", "build"], cwd=str(ROOT))
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "."], cwd=str(ROOT))
+
+
+def setup(app):
+    app.connect("config-inited", on_config_inited)
