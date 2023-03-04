be612fbbd97a9b8a30256ae836c914871de1a66f
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-03-04 10:23:28 +0000

    Fix pip install command

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@ def on_config_inited(*args):
 
     HERE = Path(__file__)
     ROOT = HERE.parent.parent
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "'.[dev]'"], cwd=str(ROOT))
     subprocess.check_call(["yarn"], cwd=str(ROOT))
     subprocess.check_call(["yarn", "build"], cwd=str(ROOT))
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "."], cwd=str(ROOT))
 
 
 def setup(app):
