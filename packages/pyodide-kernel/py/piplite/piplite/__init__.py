4993cb416306db01a64719796733b4c41c302d3d
jtpio <jtpio@users.noreply.github.com>
2023-03-06 16:27:56 +0000

    Publish 0.0.2
    SHA256 hashes:
    
    jupyterlite-pyodide-kernel-0.0.2.tgz: cbad4da045ed497f0f9a60b9ed1d885855ca65842aa17121bc3c06c26c046b3f
    
    jupyterlite-pyodide-kernel-extension-0.0.2.tgz: 8fa812b38d467b3cd746ca22596e0c667980d35a5f81aacbeb87ae28ad4cfe51
    
    jupyterlite_pyodide_kernel-0.0.2-py3-none-any.whl: 78b0cee64157b12ed06d3bf1f4b5716c288315e4080828604211c0c08f01c482
    
    jupyterlite_pyodide_kernel-0.0.2.tar.gz: 89548f76791231e22cd48783f147e94cbf7d0c23e370f69c9f76d00a6e9445cf

--- packages/pyodide-kernel/py/piplite/piplite/__init__.py
+++ packages/pyodide-kernel/py/piplite/piplite/__init__.py
@@ -1,6 +1,6 @@
 """A configurable Python package backed by Pyodide's micropip"""
 from .piplite import install
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = ["install", "__version__"]
