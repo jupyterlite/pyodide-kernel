45e7daeea5b50662f646f1c9a550de9d17391946
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-03-10 18:15:13 +0100

    Releaser: support bumping versions with `next` as the spec (#26)
    * Support `next` spec for incremental releases
    
    * Fix bump version script
    
    * Lint
    
    * Update to v3 actions

--- scripts/bump-version.py
+++ scripts/bump-version.py
@@ -3,10 +3,13 @@
 
 import argparse
 import json
+from packaging.version import parse as parse_version
 from pathlib import Path
 from subprocess import run
 
+
 ENC = dict(encoding="utf-8")
+HATCH_VERSION = "hatch version"
 ROOT = Path(__file__).parent.parent
 PYODIDE_KERNEL_PACKAGE = ROOT / "packages" / "pyodide-kernel"
 PYODIDE_PACKAGE_JSON = PYODIDE_KERNEL_PACKAGE / "package.json"
@@ -14,18 +17,30 @@ PYODIDE_KERNEL_PY_PACKAGE = PYODIDE_KERNEL_PACKAGE / "py" / "pyodide-kernel"
 PIPLITE_PY_PACKAGE = PYODIDE_KERNEL_PACKAGE / "py" / "piplite"
 
 
+def get_version():
+    cmd = run([HATCH_VERSION], capture_output=True, shell=True, check=True, cwd=ROOT)
+    return cmd.stdout.decode("utf-8").strip().split("\n")[-1]
+
+
+def next_version():
+    v = parse_version(get_version())
+    if v.is_prerelease:
+        return f"{v.major}.{v.minor}.{v.micro}{v.pre[0]}{v.pre[1] + 1}"
+    return f"{v.major}.{v.minor}.{v.micro + 1}"
+
+
 def bump():
     parser = argparse.ArgumentParser()
     parser.add_argument("version")
     args = parser.parse_args()
-    py_version = args.version
+    py_version = next_version() if args.version == "next" else args.version
     js_version = (
         py_version.replace("a", "-alpha.").replace("b", "-beta.").replace("rc", "-rc.")
     )
 
     # bump the Python version with hatch for each package
     for package in [ROOT, PYODIDE_KERNEL_PY_PACKAGE, PIPLITE_PY_PACKAGE]:
-        run(f"hatch version {py_version}", shell=True, check=True, cwd=package)
+        run(f"{HATCH_VERSION} {py_version}", shell=True, check=True, cwd=package)
 
     # bump the js version
     pyodide_kernel_json = json.loads(PYODIDE_PACKAGE_JSON.read_text(**ENC))
