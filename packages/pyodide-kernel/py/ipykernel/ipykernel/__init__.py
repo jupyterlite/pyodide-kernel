0df94b5439914fbc4a40a133f6de9f753ba5f2d7
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-08-03 08:49:17 +0200

    Add `version_info` to the `ipykernel` mock (#53)
    * Add `version_info` to the `ipykernel` mock
    
    * add missing import
    
    * missing import

--- packages/pyodide-kernel/py/ipykernel/ipykernel/__init__.py
+++ packages/pyodide-kernel/py/ipykernel/ipykernel/__init__.py
@@ -3,4 +3,16 @@
 __version__ = "6.9.2"
 __all__ = ["Comm", "CommManager", "__version__"]
 
+import re
+from typing import List
+
 from .comm import Comm, CommManager
+
+# Build up version_info tuple for backwards compatibility
+pattern = r"(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)"
+match = re.match(pattern, __version__)
+assert match is not None
+parts: List[object] = [int(match[part]) for part in ["major", "minor", "patch"]]
+if match["rest"]:
+    parts.append(match["rest"])
+version_info = tuple(parts)
