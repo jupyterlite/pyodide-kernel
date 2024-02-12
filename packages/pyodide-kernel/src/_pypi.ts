a74b99d282528b2b5f695f8e06def456d1edacc3
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-02-12 02:31:16 -0500

    Bump widgetsnbextension shim to 4.0.10 (#86)
    * bump widgetsnbextension shim
    
    * bump widgetsnbextension shim
    
    * add latex labels to widget example

--- packages/pyodide-kernel/src/_pypi.ts
+++ packages/pyodide-kernel/src/_pypi.ts
@@ -4,4 +4,4 @@ export * as ipykernelWheelUrl from '../pypi/ipykernel-6.9.2-py3-none-any.whl';
 export * as pipliteWheelUrl from '../pypi/piplite-0.2.2-py3-none-any.whl';
 export * as pyodide_kernelWheelUrl from '../pypi/pyodide_kernel-0.2.2-py3-none-any.whl';
 export * as widgetsnbextensionWheelUrl from '../pypi/widgetsnbextension-3.6.6-py3-none-any.whl';
-export * as widgetsnbextensionWheelUrl1 from '../pypi/widgetsnbextension-4.0.9-py3-none-any.whl';
+export * as widgetsnbextensionWheelUrl1 from '../pypi/widgetsnbextension-4.0.10-py3-none-any.whl';
