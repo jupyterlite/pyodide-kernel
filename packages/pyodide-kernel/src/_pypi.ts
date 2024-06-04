2d2075abad925e2169f3df96e4255be0c216baf0
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2024-06-04 16:31:58 +0200

    Backport PR #110: Bump `widgetsnbextension` to 4.0.11 (#111)
    Co-authored-by: Jeremy Tuloup <jeremy.tuloup@gmail.com>

--- packages/pyodide-kernel/src/_pypi.ts
+++ packages/pyodide-kernel/src/_pypi.ts
@@ -4,4 +4,4 @@ export * as ipykernelWheelUrl from '../pypi/ipykernel-6.9.2-py3-none-any.whl';
 export * as pipliteWheelUrl from '../pypi/piplite-0.3.1-py3-none-any.whl';
 export * as pyodide_kernelWheelUrl from '../pypi/pyodide_kernel-0.3.1-py3-none-any.whl';
 export * as widgetsnbextensionWheelUrl from '../pypi/widgetsnbextension-3.6.6-py3-none-any.whl';
-export * as widgetsnbextensionWheelUrl1 from '../pypi/widgetsnbextension-4.0.10-py3-none-any.whl';
+export * as widgetsnbextensionWheelUrl1 from '../pypi/widgetsnbextension-4.0.11-py3-none-any.whl';
