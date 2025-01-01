fb11901271a4cc627100838edb859f3e1d7ad0c6
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-12-31 22:21:11 -0600

    rework piplite

--- jupyterlite_pyodide_kernel/constants.py
+++ jupyterlite_pyodide_kernel/constants.py
@@ -1,6 +1,6 @@
 """Well-known (and otherwise) constants used by ``jupyterlite-pyodide-kernel``"""
 
-### pyodide-specific values
+### Pyodide-specific values
 #: the key for PyPI-compatible API responses pointing to wheels
 PIPLITE_URLS = "pipliteUrls"
 DISABLE_PYPI_FALLBACK = "disablePyPIFallback"
@@ -10,9 +10,9 @@ PIPLITE_INDEX_SCHEMA = "piplite.v0.schema.json"
 KERNEL_SETTINGS_SCHEMA = "kernel.v0.schema.json"
 #: where we put wheels, for now
 PYPI_WHEELS = "pypi"
-#: the plugin id for the pydodide kernel labextension
+#: the plugin id for the Pydodide kernel labextension
 PYODIDE_KERNEL_PLUGIN_ID = "@jupyterlite/pyodide-kernel-extension:kernel"
-#: the npm name of the pyodide kernel
+#: the npm name of the Pyodide kernel
 PYODIDE_KERNEL_NPM_NAME = PYODIDE_KERNEL_PLUGIN_ID.split(":")[0]
 #: the package.json key for piplite
 PKG_JSON_PIPLITE = "piplite"
@@ -34,7 +34,15 @@ PYODIDE_VERSION = "0.27.0"
 #: the only kind of noarch wheel piplite understands
 NOARCH_WHL = "py3-none-any.whl"
 
-#: the only kind of binary wheel piplite understands
-WASM_WHL = "emscripten_*_wasm32.whl"
+#: the only kind of binary wheel piplite previously understood
+EMSCRIPTEN_ABI_WHL = "emscripten_*_wasm32.whl"
 
-ALL_WHL = [NOARCH_WHL, WASM_WHL]
+#: legacy variable alias
+WASM_WHL = EMSCRIPTEN_ABI_WHL
+
+#: the Pyodide ABI wheel is the same as the Emscripten
+#: ABI wheel, but with a different platform tag, i.e.,
+#  YYYY_buildnumber.
+PYODIDE_ABI_WHL = "pyodide_*_wasm32.whl"
+
+ALL_WHL = [NOARCH_WHL, WASM_WHL, PYODIDE_ABI_WHL]
