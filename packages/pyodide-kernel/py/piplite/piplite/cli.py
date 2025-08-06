853fa9aee815fa9388f2a24f5efd475c962a7751
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-08-06 12:21:42 -0400

    Update to jupyterlite-core 0.7.x, pyodide 0.28.1, micropip 0.10.1 (#203)
    * start bumping to pyodide 0.28.0
    
    * mostly fix piplite
    
    * linting
    
    * update piplite cli options
    
    * pyodide 0.28.1
    
    * rework readme changes
    
    * commit yarn.lock
    
    * target jupyterlite-core 0.7.0a1
    
    * also bump ui-tests versions
    
    * fix more pins
    
    * more lab bumps

--- packages/pyodide-kernel/py/piplite/piplite/cli.py
+++ packages/pyodide-kernel/py/piplite/piplite/cli.py
@@ -2,39 +2,46 @@
 
 As of the upstream:
 
-    https://github.com/pyodide/micropip/blob/v0.2.0/micropip/_micropip.py#L468
+    https://github.com/pyodide/micropip/blob/0.10.0/micropip/package_manager.py#L43-L55
 
 .. code:
 
     async def install(
         self,
-        requirements: str | list[str],                  # -r and [packages]
+        requirements: str | list[str],                  # -r and [PACKAGES]
         keep_going: bool = False,                       # --verbose
         deps: bool = True,                              # --no-deps
         credentials: str | None = None,                 # no CLI alias
         pre: bool = False,                              # --pre
         index_urls: list[str] | str | None = None,      # no CLI alias
         *,
-        verbose: bool | int | None = None,
-    ):
-```
+        constraints: list[str] | None = None,           # --constraints
+        reinstall: bool = False,                        # no CLI alias
+        verbose: bool | int | None = None,              # --verbose
+    ) -> None:
 
 As this is _not_ really a CLI, it doesn't bother with accurate return codes, and
 failures should not block execution.
 """
 
+from __future__ import annotations
+
 import re
 import sys
-import typing
+from typing import Any, TYPE_CHECKING
 from argparse import ArgumentParser
 from pathlib import Path
 
-REQ_FILE_PREFIX = r"^(-r|--requirements)\s*=?\s*(.*)\s*"
+if TYPE_CHECKING:
+    from collections.abc import AsyncIterator
+
+REQ_FILE_SPEC = r"^(?P<flag>-r|--requirements)\s*=?\s*(?P<path_ref>.+)$"
 
 __all__ = ["get_transformed_code"]
 
 
-def warn(msg):
+def warn(msg: str) -> None:
+    """Print a warning to stderr."""
     print(msg, file=sys.stderr, flush=True)
 
 
@@ -44,7 +51,7 @@ def _get_parser() -> ArgumentParser:
         "piplite",
         exit_on_error=False,
         allow_abbrev=False,
-        description="a pip-like wrapper for `piplite` and `micropip`",
+        description="a ``pip``-like wrapper for ``piplite`` and ``micropip``",
     )
     parser.add_argument(
         "--verbose",
@@ -64,7 +71,19 @@ def _get_parser() -> ArgumentParser:
         "--requirements",
         "-r",
         nargs="*",
-        help="paths to requirements files",
+        help=(
+            "path to a requirements file; each line should be a PEP 508 spec"
+            " or -r to a relative path"
+        ),
+    )
+    parser.add_argument(
+        "--constraints",
+        "-c",
+        nargs="*",
+        help=(
+            "path to a constraints file; each line should be a PEP 508 spec"
+            " or -r to a relative path"
+        ),
     )
     parser.add_argument(
         "--no-deps",
@@ -77,6 +96,11 @@ def _get_parser() -> ArgumentParser:
         help="whether pre-release packages should be considered",
     )
     parser.add_argument(
+        "--force-reinstall",
+        action="store_true",
+        help="reinstall all packages even if they are already installed",
+    )
+    parser.add_argument(
         "packages",
         nargs="*",
         type=str,
@@ -87,21 +111,25 @@ def _get_parser() -> ArgumentParser:
     return parser
 
 
-async def get_transformed_code(argv: list[str]) -> typing.Optional[str]:
+async def get_transformed_code(argv: list[str]) -> str | None:
     """Return a string of code for use in in-kernel execution."""
     action, kwargs = await get_action_kwargs(argv)
+    code_str: str = "\n"
 
     if action == "help":
         pass
+
     if action == "install":
         if kwargs["requirements"]:
-            return f"""await __import__("piplite").install(**{kwargs})\n"""
+            code_str = f"""await __import__("piplite").install(**{kwargs})\n"""
         else:
             warn("piplite needs at least one package to install")
 
+    return code_str
 
-async def get_action_kwargs(argv: list[str]) -> tuple[typing.Optional[str], dict]:
-    """Get the arguments to `piplite` subcommands from CLI-like tokens."""
+
+async def get_action_kwargs(argv: list[str]) -> tuple[str | None, dict[str, Any]]:
+    """Get the arguments to ``piplite`` subcommands from CLI-like tokens."""
 
     parser = _get_parser()
 
@@ -126,51 +154,54 @@ async def get_action_kwargs(argv: list[str]) -> tuple[typing.Optional[str], dict
         if args.verbose:
             kwargs["keep_going"] = True
 
+        if args.reinstall:
+            kwargs["reinstall"] = True
+
         for req_file in args.requirements or []:
-            kwargs["requirements"] += await _packages_from_requirements_file(
-                Path(req_file)
-            )
+            async for spec in _specs_from_requirements_file(Path(req_file)):
+                kwargs["requirements"] += [spec]
+
+        for const_file in args.constraints or []:
+            async for spec in _specs_from_requirements_file(Path(const_file)):
+                kwargs["constraints"] += [spec]
 
     return action, kwargs
 
 
-async def _packages_from_requirements_file(req_path: Path) -> list[str]:
-    """Extract (potentially nested) package requirements from a requirements file."""
-    if not req_path.exists():
-        warn(f"piplite could not find requirements file {req_path}")
-        return []
+async def _specs_from_requirements_file(spec_path: Path) -> AsyncIterator[str]:
+    """Extract package specs from a ``requirements.txt``-style file."""
+    if not spec_path.exists():
+        warn(f"piplite could not find requirements file {spec_path}")
+        return
 
-    requirements = []
-
-    for line_no, line in enumerate(req_path.read_text(encoding="utf").splitlines()):
-        requirements += await _packages_from_requirements_line(
-            req_path, line_no + 1, line
-        )
-
-    return requirements
+    for line_no, line in enumerate(spec_path.read_text(encoding="utf").splitlines()):
+        async for spec in _specs_from_requirements_line(spec_path, line_no + 1, line):
+            yield spec
 
 
-async def _packages_from_requirements_line(
-    req_path: Path, line_no: int, line: str
-) -> list[str]:
-    """Extract (potentially nested) package requirements from line of a
-    requirements file.
+async def _specs_from_requirements_line(
+    spec_path: Path, line_no: int, line: str
+) -> AsyncIterator[str]:
+    """Get package specs from a line of a ``requirements.txt``-style file.
 
-    `micropip` has a sufficient pep508 implementation to handle most cases
+    ``micropip`` has a sufficient pep508 implementation to handle most cases.
+
+    References to other, local files with ``-r`` are supported.
     """
-    req = line.strip().split("#")[0].strip()
-    # is it another requirement file?
-    req_file_match = re.match(REQ_FILE_PREFIX, req)
-    if req_file_match:
-        if req_file_match[2].startswith("/"):
-            sub_req = Path(req)
-        else:
-            sub_req = req_path.parent / req_file_match[2]
-        return await _packages_from_requirements_file(sub_req)
+    raw = line.strip().split("#")[0].strip()
+    # is it another spec file?
+    file_match = re.match(REQ_FILE_SPEC, raw)
 
-    if req.startswith("-"):
-        warn(f"{req_path}:{line_no}: unrecognized requirement: {req}")
-        req = None
-    if not req:
-        return []
-    return [req]
+    if file_match:
+        ref = file_match.groupdict()["path_ref"]
+        ref_path = Path(ref if ref.startswith("/") else spec_path.parent / ref)
+        async for sub_spec in _specs_from_requirements_file(ref_path):
+            yield sub_spec
+    elif raw.startswith("-"):
+        warn(f"{spec_path}:{line_no}: unrecognized spec: {raw}")
+        return
+    else:
+        spec = raw
+
+    if spec:
+        yield spec
